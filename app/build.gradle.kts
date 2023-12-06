9aff997ec4663183eae250caf98b7e58cf02046e
wlsdlwlsdlwl <wlsdlwlsdlwl@naver.com>
2023-12-06 16:42:13 +0900

    AsyncImage 및 Card 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
--- app/src/main/java/kr/ac/kumoh/ce/s20200663/s23w13songlist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200663/s23w13songlist/MainActivity.kt
@@ -4,25 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import kr.ac.kumoh.ce.s20200663.s23w13songlist.ui.theme.S23W13SongListTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,22 +68,51 @@ fun MainScreen(viewModel: SongViewModel) {
 @Composable
 fun SongList(list: List<Song>) {
     LazyColumn (
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
-        items(list) { song ->
-            SongItem(song)
+        itemsIndexed(list) { index, song ->
+            SongItem(index, song)
         }
     }
 }
 
 @Composable
-fun SongItem(song: Song) {
-    Column(
-        modifier = Modifier.fillMaxWidth().background(Color(0xffffffcc)).padding(16.dp)
+fun SongItem(index: Int, song: Song) {
+    var expanded by remember { mutableStateOf(false)}
+
+    Card(
+        modifier = Modifier.clickable { expanded = !expanded},
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        TextTitle(song.title)
-        TextSinger(song.singer)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                //.background(Color(255, 210, 210))
+                .padding(8.dp)
+        ) {
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=$index",
+                contentDescription = "노래 앨범 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(100.dp)
+                    //.clip(CircleShape),
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                //verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                TextTitle(song.title)
+                TextSinger(song.singer)
+            }
+        }
+        AnimatedVisibility(visible = expanded) {
+            song.lyrics?.let{Text(it)}
+        }
     }
 }
 
